8b928306db9968a5ae02fe3113be0c37553d74da
johirjoy <joy.sd69@gmail.com>
2023-04-18 01:07:06 +0600

    complete project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
 
 const Header = () => {
+    const {user, logOut} = useContext(AuthContext)
+
+    const handleLoOut =() =>{
+        logOut()
+        .then(result => {})
+        .catch(error => console.error(error))
+    }
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -12,6 +21,10 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
+                <Link to="/sign-up">Sign Up</Link>
+                {
+                    user && <span className='text-white' >Welcome {user.email} <button onClick={handleLoOut} >Sign Out</button> </span>
+                }
             </div>
         </nav>
     );
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBggSpHyRU4kn4dthpQlIpomvZjEsDgj94",
+  authDomain: "ema-john-with-firebase-a-a0cde.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-a0cde",
+  storageBucket: "ema-john-with-firebase-a-a0cde.appspot.com",
+  messagingSenderId: "364726094603",
+  appId: "1:364726094603:web:de4559518b92587144300e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
